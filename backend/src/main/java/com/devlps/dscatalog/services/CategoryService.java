edccabe9ac61f2fc8d0d80733c0d739d1d449bdd
lorivalSouza <lorivalnew@gmail.com>
2021-09-16 11:21:35 -0300

    Class DTO

--- backend/src/main/java/com/devlps/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devlps/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devlps.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devlps.dscatalog.dto.CategoryDTO;
 import com.devlps.dscatalog.entities.Category;
 import com.devlps.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,13 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();			
+		
+		//To understand, return same. Return listDTO
+		//List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	}
 
