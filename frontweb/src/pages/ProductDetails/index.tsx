f63d365a1dd2fe44dd0a329dcc56c107e03e4027
lorivalSouza <lorivalnew@gmail.com>
2021-11-12 14:44:46 -0300

    ProductDatail - Catalog  Links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -8,10 +9,13 @@ const ProductDetails = () => {
     <>
       <div className="product-details-container">
         <div className="base-card product-details-card">
-          <div className="goback-container">
-            <ArrowIcon />
-            <h2>Voltar</h2>
-          </div>
+          <Link to="/products">
+            <div className="goback-container">
+              <ArrowIcon />
+              <h2>Voltar</h2>
+            </div>
+          </Link>
+
           <div className="row">
             <div className="col-xl-6">
               <div className="img-container">
