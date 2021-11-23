c65f8cb8f9b3d96ff2c5c292f26837dcc03e043f
lorivalSouza <lorivalnew@gmail.com>
2021-11-23 10:36:20 -0300

    Function hasAnyHole

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
+import { hasAnyRoly } from 'util/requests';
 
 import './styles.css';
 
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <>
       <div className="home-container">
+        <h1>Resultado= {hasAnyRoly(['ROLE_ADMIN']) ? 'sim' : 'não'}</h1>
         <div className="base-card home-card">
           <div className="home-content-container">
             <div>
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -115,5 +115,25 @@ export const getTokenData = (): TokenData | undefined => {
 export const isAuthenticated = (): boolean => {
   const tokenData = getTokenData();
 
-  return (tokenData && (tokenData.exp * 1000) > Date.now()) ? true : false;
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
+
+export const hasAnyRoly = (roles: Role[]): boolean => {
+  if (roles.length === 0) {
+    return true;
+  }
+  const tokendata = getTokenData();
+
+  if (tokendata !== undefined) {
+    return roles.some(role => tokendata.authorities.includes(role));
+  }
+/* optional to function 
+  if (tokendata !== undefined) {
+    for(var i =0; i < roles.length; i++){
+      if(tokendata.authorities.includes(roles[i])){
+        return true;
+      }
+    }
+  } */
+  return false;
 };
