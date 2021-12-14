45741d4dc3c371b6c396448921e00ae946348f37
lorivalSouza <lorivalnew@gmail.com>
2021-12-14 10:51:00 -0300

    Products routes

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,9 +1,10 @@
 import PrivateRoute from 'components/PrivateRoute';
 import { Switch } from 'react-router-dom';
 import Navbar from './Navbar';
+import Products from './Products';
+import Users from './Users';
 
 import './styles.css';
-import Users from './Users';
 
 const Admin = () => {
   return (
@@ -12,7 +13,7 @@ const Admin = () => {
       <div className="admin-content">
         <Switch>
           <PrivateRoute path="/admin/products">
-            <h1>Product crud</h1>
+            <Products />
           </PrivateRoute>
           <PrivateRoute path="/admin/categories">
             <h1>Categories crud</h1>
