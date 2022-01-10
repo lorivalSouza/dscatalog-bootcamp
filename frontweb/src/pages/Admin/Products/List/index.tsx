20ea1eb8099aedbe4543a402e947c711a85da05d
lorivalSouza <lorivalnew@gmail.com>
2022-01-10 11:19:43 -0300

    Delete product and observer patterns

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -13,6 +13,10 @@ const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
@@ -24,7 +28,7 @@ const List = () => {
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  }, []);
+  };
 
   return (
     <div className="product-crud-container">
@@ -38,9 +42,9 @@ const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        {page?.content.map(product => (
+        {page?.content.map((product) => (
           <div key={product.id} className="col-sm-6 col-md-12">
-            <ProductCrudCard product={product} />
+            <ProductCrudCard product={product} onDelete={() => getProducts()} />
           </div>
         ))}
       </div>
--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -1,15 +1,33 @@
+import { AxiosRequestConfig } from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+import { requestBackend } from 'util/requests';
 import CategoryBadge from '../CategoryBadge';
 
 import './styles.css';
 
 type Props = {
   product: Product;
+  onDelete: Function;
 };
 
-const ProductCrudCard = ({ product }: Props) => {
+const ProductCrudCard = ({ product, onDelete }: Props) => {
+  const handleDelete = (productId: number) => {
+    if (!window.confirm('Tem certeza que deseja deletar?')) {
+      return;
+    }
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/products/${productId}`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      onDelete();
+    });
+  };
+
   return (
     <div className="base-card product-crud-card">
       <div className="product-crud-card-top-container">
@@ -27,13 +45,16 @@ const ProductCrudCard = ({ product }: Props) => {
         </div>
       </div>
       <div className="product-crud-card-buttons-container">
-        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+        <button
+          onClick={() => handleDelete(product.id)}
+          className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first"
+        >
           Excluir
         </button>
         <Link to={`/admin/products/${product.id}`}>
-        <button className="btn btn-outline-secondary product-crud-card-button">
-          Editar
-        </button>
+          <button className="btn btn-outline-secondary product-crud-card-button">
+            Editar
+          </button>
         </Link>
       </div>
     </div>
