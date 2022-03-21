f2aa38beac69d1f4bb7558f87e4c0ffe18d1e0cd
lorivalSouza <lorivalnew@gmail.com>
2022-03-21 10:07:55 -0300

    Mock function with spyOn

---
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,38 @@
+import { hasAnyRoles } from '../auth';
+import * as TokenModule from '../token';
+
+describe(' has any role tests', () => {
+
+    test('should return true when empty list', () => {
+        const result = hasAnyRoles([]);
+
+        expect(result).toEqual(true);
+    });
+
+    test('should return true when user has given role ADMIN', () => {
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR'],
+        })
+
+        const result = hasAnyRoles(['ROLE_ADMIN']);
+
+        expect(result).toEqual(true);
+    });
+
+    test('should return true when user has given role OPERATOR', () => {
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR'],
+        })
+
+        const result = hasAnyRoles(['ROLE_OPERATOR']);
+
+        expect(result).toEqual(true);
+    });
+
+});
